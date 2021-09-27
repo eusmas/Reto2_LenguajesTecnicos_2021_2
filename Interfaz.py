70d47a6e581f9e2202d4e2068174b3032710c9dc
eusmas <eus08@hotmail.com>
2021-09-27 01:59:22 -0600

    actualizacion

--- Interfaz.py
+++ Interfaz.py
@@ -1,16 +1,29 @@
 from tkinter import *
+import serial, time, sys
+
+arduino=serial.Serial('COM1', 9600)
+
+def orden(numero):
+    dato = str(numero)
+    arduino.write(dato.encode())
+    time.sleep(1)
 
 root = Tk()
 root.title("Reto #2")
-root.config(width=150,height=200)
-#root.resizable(0,0)
+root.config(width=500,height=500)
 
-#ventana_1=Frame()
-#ventana_1.pack(side=TOP)
-#ventana_1.config(bg="blue")
-#ventana_1.config(width="250",height="250")
-titulo=Label(root,text="Reto#2 Lenguajes Tecnicos 2021-2",bg="red")
-titulo.place(x=25,y=0)
+Label(root,text="Reto#2 Lenguajes Tecnicos 2021-2",bg="blue").grid(row=0,column=0,columnspan=5,sticky=W+E)
+Label(root,text="Escoja el piso al cual desplazarse").grid(row=2,column=0,columnspan=5,sticky=W+E)
+
+Button(root,text="4",command=orden(4)).grid(row=4,column=1,sticky=W+E)
+Button(root,text="3",command=orden(3)).grid(row=5,column=1,sticky=W+E)
+Button(root,text="2",command=orden(2)).grid(row=6,column=1,sticky=W+E)
+Button(root,text="1",command=orden(1)).grid(row=7,column=1,sticky=W+E)
+
+
+print ("Ingrese 1 para encender el led y 0 para apaga el led")
+dato = str(input())
+arduino.write(dato.encode())
 
 root.mainloop()
 
